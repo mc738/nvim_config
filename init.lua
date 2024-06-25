77c7fcf07b9aa900795ef464d7f937db74f85db8
max <me@mclifford.dev>
2024-06-25 19:00:20 +0100

    Work on plugin configs

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
 require("config.lazy")
+
+require("mason").setup({
+    ui = {
+	    icons = {
+		    package_installed = ""
+		    package_pending = ""
+		    package_uninstalled = ""
+	    },
+    }
+})
+require("main-lspconfig").setup()
