94f890ec0b9832deb22274de466b46099a8178bb
max <me@mclifford.dev>
2024-06-26 10:35:11 +0100

    Added nvim-treesitter config

--- init.lua
+++ init.lua
@@ -109,4 +109,18 @@ cmp.setup({
 	},
 })
 
-
+-- Treesitter Plugin setup
+require('nvim-treesitter.configs').setup {
+        ensure_installed = { "lua", "rust", "toml" },
+	auto_install = true,
+	highlight = {
+	        enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = { enable = true },
+	rainbow = {
+	    enable = true,
+	    extended_mode = true,
+	    max_file_lines = nil,
+	}
+}
